f45419c523c4054775e777eaf9f2046cd3e8c60a
Anders Johansson <trumanders@gmail.com>
2023-06-02 08:36:45 +0200

    made private calculate method

--- RefactoringExcersize/CalculatorStack.cs
+++ RefactoringExcersize/CalculatorStack.cs
@@ -15,22 +15,25 @@ public class CalculatorStack
             if (!Double.TryParse(input, out double num)) return false;
             stack.Push(num);
             return true;
-        }
-        
+        }        
         else if (input.Length != 1) return false;
         else if (input[0] == 'c') { stack.Clear(); return true; }
-        else if (stack.Count > 1)
-        {
-            double first = stack.Pop();
-            double second = stack.Pop();
-            switch (input[0])
-            {
-                case '+': stack.Push(first + second); return true;
-                case '*': stack.Push(first * second); return true;
-                case '-': stack.Push(second - first); return true;
-                case '/': stack.Push(second / first); return true;
-            }            
-        }
+        else if (stack.Count > 1 && performCalculation(input[0]))
+            return true;                             
         return false;
     }
+
+    private bool performCalculation(char operatr)
+    {
+        double firstNum = stack.Pop();
+        double secondNum = stack.Pop();
+        switch (operatr)
+        {
+            case '+': stack.Push(firstNum + secondNum); return true;
+            case '*': stack.Push(firstNum * secondNum); return true;
+            case '-': stack.Push(secondNum - firstNum); return true;
+            case '/': stack.Push(secondNum / firstNum); return true;
+        }
+        return false;
+    }   
 }
